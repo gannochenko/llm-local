1f73087a073c8204705a185467e705db02c7a828
Sergei Gannochenko <gannochenko.dev@gmail.com>
2025-03-21 01:57:24 +0100

    wip

--- apps/monitor/dist/metrics/metrics.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { MetricsService } from "./metrics.service";
-export declare class MetricsController {
-    private readonly metricsService;
-    constructor(metricsService: MetricsService);
-    getMetrics(): Promise<string>;
-    getInfo(): string;
-}
